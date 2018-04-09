a215d9db0c358cab28820511bdcdd34432ac57ce
Jake Howard <git@theorangeone.net>
2018-04-09 21:42:03 +0100

    Add custom config

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@
 
 extern crate rocket;
 
+use rocket::config::{Config, Environment};
+
+
 #[get("/<org>/<repo>/<path>")]
 fn handle(org: String, repo: String, path: String) -> String {
     return "woo".into();
 }
 
+
 fn main() {
-    rocket::ignite().mount("/", routes![handle]).launch();
+    let config = Config::build(Environment::Development)
+        .port(5000)
+        .finalize().unwrap();
+    rocket::custom(config, true).mount("/", routes![handle]).launch();
 }
