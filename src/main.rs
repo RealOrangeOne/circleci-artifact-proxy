488837205ef5dafd507ddbb72fb8ef52e8f5dec9
Jake Howard <git@theorangeone.net>
2018-04-09 21:44:15 +0100

    Run formatter

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ extern crate rocket;
 
 use rocket::config::{Config, Environment};
 
-
 #[get("/<org>/<repo>/<path>")]
 fn handle(org: String, repo: String, path: String) -> String {
     return "woo".into();
 }
 
-
 fn main() {
     let config = Config::build(Environment::Development)
         .port(5000)
-        .finalize().unwrap();
-    rocket::custom(config, true).mount("/", routes![handle]).launch();
+        .finalize()
+        .unwrap();
+    rocket::custom(config, true)
+        .mount("/", routes![handle])
+        .launch();
 }
